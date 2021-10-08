226bcb06bf13f9d5dc9d0cd2675c1c83f9101c9b
Daniel Xie <dyxie@purdue.edu>
2021-10-08 00:14:40 -0400

    Add files via upload

---
+++ hoops.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int check1 = 0;
+int check2 = 0;
+
+void win() {
+	if (check1 && check2) {
+		printf("win! :)\n");
+	} else {
+		printf("you need to jump through more hoops first :(");
+	}
+}
+
+
+void hoop1() {
+	printf("jumped through hoop 1!\n");
+	check1 = 1;
+}
+
+void hoop2(int x) {
+	if (x == 0x1337) {
+		printf("jumped through hoop 2!\n");
+		check2 = 1;
+	}
+}
+
+void vuln() {
+	char buf[8];
+	printf("jump through the hoops!\n");
+	gets(buf);
+
+}
+
+void main() {
+	vuln();
+}
+
